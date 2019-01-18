fd5966b3ac8ebd5c3e0e04123dd75d6eca05f979
Barret Schloerke <schloerke@gmail.com>
2019-01-18 12:25:11 -0500

    remove comments

--- scripts/questions.R
+++ scripts/questions.R
@@ -1,13 +1,9 @@
 library(shiny)
 options("shiny.reactlog" = TRUE)
 
-# input$audience (ready) -> enjoy presentation (ready) -> output$askQuestion (calculating)
-# answer (ready) -> speaker (calculating) -> output$askQuestion (calculating)
-
 
 shinyApp(
   fluidPage(
-    # numericInput("size", "Audience Size:", 150),
     textInput("questions", "Question:", "many"),
     "Ask Questions: ", textOutput("Q&A")
   ),
